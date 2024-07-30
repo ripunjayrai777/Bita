130e3526866e434474c11afbc66ef1f4af96bd17
RIPUNJAY RAI <111183094+ripunjayrai777@users.noreply.github.com>
2024-07-30 15:54:35 +0530

    Add files via upload

---
+++ include/footer.php
@@ -0,0 +1,30 @@
+<!-- Footer -->
+
+<div class="container-fluid bg-white mt-5">
+    <div class="row">
+      <div class="col-lg-4 p-4">
+      <h3 class="h-font fw-bold fs-3 mb-2">BOBINA HOTEL</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae doloribus placeat ab ex? Earum distinctio expedita dolor, sed eligendi excepturi.</p>
+      </div>
+      <div class="col-lg-4 p-4">
+      <h5 class="mb-3">Links</h5>
+      <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Home</a><br>
+      <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Rooms</a><br>
+      <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Facilities</a><br>
+      <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Contact Us</a><br>
+      <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Abouts</a>
+      </div>
+      <div class="col-lg-4 p-4">
+        <h5 class="mb-3">Follow Us</h5>
+        <a href="#" class="d-inline-block text-dark text-decoration-none mb-2"><i class="bi bi-twitter"></i> Twitter</a><br>
+        <a href="#" class="d-inline-block text-dark text-decoration-none mb-2"><i class="bi bi-facebook"></i> Facebook</a><br>
+        <a href="#" class="d-inline-block text-dark text-decoration-none "><i class="bi bi-instagram"></i> Instagram</a>
+      </div>
+    </div>
+  </div>
+  
+  <!-- Designed and Developed by Ripunjay -->
+  
+  <h6 class="text-center bg-dark text-white p-3 m-0">Designed and Developed by Ripunjay Rai</h6>
+  
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
